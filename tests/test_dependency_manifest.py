f3e6c93cd24c07b71e77f1bf5e14bdd78ca35ecb
ROMERUU-dev <juvenal.romero@uabc.edu.mx>
2026-04-10 13:32:12 -0700

    Point bundled PDK manifest to beta 3 assets

--- tests/test_dependency_manifest.py
+++ tests/test_dependency_manifest.py
@@ -24,8 +24,8 @@ class DependencyManifestTest(unittest.TestCase):
         self.assertEqual(channel.pdk_bundle_install_root, "~/pdk")
         self.assertEqual(channel.pdk_bundle_name, "tt-pdk-sky130a")
         self.assertTrue(channel.pdk_bundle_enabled)
-        self.assertEqual(channel.pdk_bundle_version, "0.2.0-beta.2")
-        self.assertTrue(channel.pdk_bundle_asset_url.endswith("tt-pdk-sky130a_0.2.0-beta.2.tar.gz"))
+        self.assertEqual(channel.pdk_bundle_version, "0.2.0-beta.3")
+        self.assertTrue(channel.pdk_bundle_asset_url.endswith("tt-pdk-sky130a_0.2.0-beta.3.tar.gz"))
 
     def test_unknown_channel_raises(self) -> None:
         manifest = DependencyManifest()
